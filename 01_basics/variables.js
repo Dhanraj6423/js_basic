8c87bab2ac20030dd936a84b243b951bd006f3c0
Dhanraj6423 <dhanrajnimbolkar@gmail.com>
2024-08-03 22:41:07 +0530

    initial commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+console.log("hii")
+const acctId="1234"
+let acctMail="mlm@gmail.com"
+var acctcity="Rampur"
+acctpassword= "@rerenode"
+console.log(acctId);
+
+acctMail="bdb@gmail.com"
+acctcity="kanpur"
+acctpassword= "@rerende"
+
+console.table ([acctId,acctMail,acctcity])
+//comment
+/**hdfkjdjfdak;fh; */
